dc64be75de64b90ecd6623aa605ff49467a1552d
sam7075938131 <sam7075938131@gmail.com>
2022-12-26 12:37:32 +0530

    Added profileDetails page
    Co-Authored-By: sanyuraj04 <116296042+sanyuraj04@users.noreply.github.com>

--- lib/screens/Student/MyProfilePage/ProfileDetailsPage.dart
+++ lib/screens/Student/MyProfilePage/ProfileDetailsPage.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import '../../../customs/theme.dart';
+import '../../../utils/utils.dart';
 
 class ProfileDetailsPage extends StatefulWidget {
   const ProfileDetailsPage({super.key});
@@ -25,55 +28,100 @@ class _ProfileDetailsPageState extends State<ProfileDetailsPage> with SingleTick
         headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
           return [
             SliverAppBar(
-              pinned: false,
+              systemOverlayStyle: const SystemUiOverlayStyle(
+                statusBarColor: Colors.black54,
+                statusBarIconBrightness: Brightness.light,
+                // For Android (dark icons)
+                statusBarBrightness: Brightness.dark, // For iOS (dark icons)
+              ),
+              floating: false,
+              snap: false,
+              stretch: true,
+              pinned: true,
+              title: Text(
+                  "My Profile",
+                style: SafeGoogleFont(
+                    'Poppins',
+                    fontWeight: FontWeight.w500,
+                    fontSize: 20,
+                    color: secondaryBlue
+                ),
+              ),
               backgroundColor: Colors.white,
               flexibleSpace: FlexibleSpaceBar(
-                collapseMode: CollapseMode.pin,
+                collapseMode: CollapseMode.parallax,
                 background: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Container(
-                      height: 200.0,
-                      width: double.infinity,
-                      padding: const EdgeInsets.only(top: 30),
-                      decoration: profileGradient,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: Text(
-                        'Samuel Philip',
-                        style: TextStyle(fontSize: 25.0),
-                        textAlign: TextAlign.left,
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: Text(
-                        '21BCE7615\nMH-2 - 405\nHosteller',
-                        style: TextStyle(fontSize: 15.0),
-                        textAlign: TextAlign.left,
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(right: 10.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: <Widget>[
-                          Icon(Icons.share),
-                          Padding(
-                            padding: const EdgeInsets.only(left: 10.0),
-                            child: Icon(Icons.favorite),
+                  children: [
+                    Column(
+                      children: <Widget>[
+
+                        Container(
+                          height: MediaQuery.of(context).size.height * 0.264,
+                          width: double.infinity,
+                          padding: const EdgeInsets.only(top: 30),
+                          decoration: profileGradient,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    "SANYU RAJ",
+                                    style: SafeGoogleFont(
+                                        'Poppins',
+                                        fontWeight: FontWeight.w600,
+                                        fontSize: 24,
+                                        color: Colors.white
+                                    ),
+                                  ),
+                                  Text(
+                                    "21BCE8381",
+                                    style: SafeGoogleFont(
+                                        'Poppins',
+                                        fontWeight: FontWeight.w500,
+                                        fontSize: 20,
+                                        color: const Color(0x74FFFFFF)
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              const CircleAvatar(
+                                backgroundColor: Colors.white,
+                                radius: 42.0,
+                                child: CircleAvatar(
+                                  radius: 36.0,
+                                  backgroundImage:
+                                  NetworkImage('https://img.freepik.com/free-photo/happy-young-female-student-holding-notebooks-from-courses-smiling-camera-standing-spring-clothes-against-blue-background_1258-70161.jpg?w=2000'),
+                                  backgroundColor: Colors.transparent,
+                                ),
+                              ),
+                            ],
                           ),
-                        ],
-                      ),
-                    )
+                        ),
+
+                      ],
+
+                    ),
                   ],
                 ),
               ),
-              expandedHeight: 380.0,
+              expandedHeight: 230.0,
               bottom: TabBar(
-                indicatorColor: Colors.black,
-                labelColor: Colors.black,
+                labelStyle: TextStyle(
+
+
+                      fontWeight: FontWeight.w700,
+                      fontSize: 12,
+                      color: primaryColor
+
+                ),
+                indicatorColor: secondaryBlue,
+                labelColor: primaryColor,
                 tabs: [
                   Tab(text: 'PERSONAL'),
                   Tab(text: 'EDUCATION'),
@@ -84,23 +132,28 @@ class _ProfileDetailsPageState extends State<ProfileDetailsPage> with SingleTick
             )
           ];
         },
-        body: ListView.builder(
-          itemCount: 100,
-          itemBuilder: (BuildContext context, int index) {
-            return Card(
-              color: index % 2 == 0 ? Colors.blue : Colors.green,
-              child: Container(
-                alignment: Alignment.center,
-                width: double.infinity,
-                height: 100.0,
-                child: Text(
-                  'Flutter is awesome',
-                  style: TextStyle(fontSize: 18.0),
+        body: Column(
+          children: [
+            Expanded(
+              child: TabBarView(
+                  controller: controller,
+                  children: [
+                Container(
+                  color: Colors.red,
+                  height: 250,
                 ),
-              ),
-            );
-          },
-        ),
+                Container(
+                  color: Colors.blueAccent,
+                  height: 250,
+                ),
+                Container(
+                  color: Colors.greenAccent,
+                  height: 250,
+                )
+              ]),
+            )
+          ],
+        )
       ),
     );
   }
